f011ac6ca4ea68d7d09cfc3c51ccbb5077f24156
kirill.lisitsin <kirill.lisitsin@lacit.net>
2021-07-30 02:50:58 +0300

    first commit

---
+++ src/main/java/org/example/data/Account.java
@@ -0,0 +1,58 @@
+package org.example.data;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Account {
+
+    private volatile Long moneyAccount;
+    private final String name;
+    private final Lock lock;
+
+    public Account(String name, long startMoney) {
+        this.name = name;
+        this.lock = new ReentrantLock(false);
+        this.moneyAccount = startMoney;
+    }
+
+    public void add(long money) {
+        synchronized (moneyAccount) {
+            this.moneyAccount += money;
+        }
+    }
+
+    public boolean minus(long money) {
+        synchronized (moneyAccount) {
+            if (this.moneyAccount - money >= 0) {
+                this.moneyAccount -= money;
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public void lock() {
+        lock.lock();
+    }
+
+    public void unlock() {
+        lock.unlock();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getMoneyAccount() {
+        return moneyAccount;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "moneyAccount=" + moneyAccount +
+                ", name='" + name +
+                '}';
+    }
+}
